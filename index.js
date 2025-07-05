441e5c32c7ca84c9678308ee6882d0cd68e265c2
Mark <mhaynes031395@gmail.com>
2025-07-05 16:01:47 -0400

    Roll on click

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// Set a random value between 1 and 6
-// Math.random() → gives a random decimal between 0 and <1.
+function rollDice(){
+    // Set a random value between 1 and 6
+    // Math.random() → gives a random decimal between 0 and <1.
 
-// Math.random() * 6 → gives a decimal between 0 and <6.
+    // Math.random() * 6 → gives a decimal between 0 and <6.
 
-// Math.floor(...) → rounds it down to an integer between 0 and 5.
+    // Math.floor(...) → rounds it down to an integer between 0 and 5.
 
-// + 1 → shifts the range to 1 through 6.
-let ranNum1 = Math.floor(Math.random() * 6) + 1;
-let ranNum2 = Math.floor(Math.random() * 6) + 1;
+    // + 1 → shifts the range to 1 through 6.
+    let ranNum1 = Math.floor(Math.random() * 6) + 1;
+    let ranNum2 = Math.floor(Math.random() * 6) + 1;
 
 // Connected the random number to a dice img
-let dice1 = "./images/dice" + ranNum1 + ".png";
-let dice2 = "./images/dice" + ranNum2 + ".png";
+    let dice1 = "./images/dice" + ranNum1 + ".png";
+    let dice2 = "./images/dice" + ranNum2 + ".png";
 
 // Update the image
-document.querySelector(".img1").setAttribute("src",dice1);
-document.querySelector(".img2").setAttribute("src",dice2);
+    document.querySelector(".img1").setAttribute("src",dice1);
+    document.querySelector(".img2").setAttribute("src",dice2);
 
 
-let winner = document.querySelector("h1");
+    let winner = document.querySelector("h1");
 
-if (ranNum1 > ranNum2){
-    winner.innerHTML = "Player 1 Wins!";
+    if (ranNum1 > ranNum2){
+        winner.innerHTML = "Player 1 Wins!";
+    }
+    else if (ranNum2 > ranNum1){
+        winner.innerHTML = "Player 2 Wins!";
+    }
+    else {
+        winner.innerHTML = "It's A Tie Noone Wins";
+    }
+
 }
-else if (ranNum2 > ranNum1){
-    winner.innerHTML = "Player 2 Wins!";
-}
-else {
-    winner.innerHTML = "It's A Tie Noone Wins";
-}
 
+document.getElementById("rollBtn").addEventListener("click", rollDice);
